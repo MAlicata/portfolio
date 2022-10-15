9aa59cdaaebc85625b426f08a406bcc128b743cf
Matias Jesus Alicata <matiasalicata@gmail.com>
2022-10-15 12:33:00 -0300

    add queries by id

--- portfolio/resources/views/posts/index.blade.php
+++ portfolio/resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
 >
     <h3>Blog 😎</h3>
     @foreach($posts as $post)
-    <h2>{{ $post->title }}</h2>       
+    <h2><a href="{{route('posts.show', $post->id)}}">{{ $post->title }}</a></h2>       
     @endforeach
 </x-layouts.app>
 {{-- @endcomponent --}}
